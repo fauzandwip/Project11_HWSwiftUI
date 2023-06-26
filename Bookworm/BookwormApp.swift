fe5dfa724f1e9db587ec2a5036e1d87e5bcd1ec8
fauzandp <fauzan27dp@gmail.com>
2023-06-26 21:04:02 +0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Fauzan Dwi Prasetyo on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
