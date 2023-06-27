1d43742a00ec5e0ba665988b44d6778cc6d4c90b
fauzandp <fauzan27dp@gmail.com>
2023-06-27 15:46:30 +0700

    Project11_HWSwiftUI | Bookworm App Showing book details
    create DetailView
    create UI

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,61 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Fauzan Dwi Prasetyo on 27/06/23.
+//
+
+import CoreData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.title ?? "Unknown Title")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No Review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+//struct DetailView_Previews: PreviewProvider {
+//    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+//
+//    static var previews: some View {
+//        let book = Book(context: moc)
+//        book.title = "Test Title"
+//        book.author = "Test Author"
+//        book.genre = "Fantasy"
+//        book.review = "Test Review"
+//        book.rating = 4
+//
+//        return NavigationView {
+//            DetailView(book: book)
+//        }
+//    }
+//}
