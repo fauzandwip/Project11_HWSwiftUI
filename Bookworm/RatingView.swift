3813d112b278994cda6d084850df41e36e9c668a
fauzandp <fauzan27dp@gmail.com>
2023-07-13 22:13:22 +0700

    Project15_HWSwiftUI | Accessibility VoiceOver fix project with Accessibility VoiceOver additional add screenshots and README

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -34,6 +34,20 @@ struct RatingView: View {
                     }
             }
         }
+        // project 15 - Accessibility VoiceOver
+        .accessibilityElement()
+        .accessibilityLabel("Rating")
+        .accessibilityValue(rating == 1 ? "1 star" : "\(rating) stars")
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            default:
+                break
+            }
+        }
     }
     
     func image(for number: Int) -> Image {
